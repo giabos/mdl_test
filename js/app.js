14873ec147b6d8d582f82c40c32e8e9107469d5f
giabos <gianni.boscarino@gmail.com>
2015-11-22 20:06:04 +0100

    initial release

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/* global jQuery */
+
+(function ($) {
+	
+	 $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+	
+	
+})(jQuery);
